f1f52be806465cce3294ad0dd05a33f8fc81958b
VootLejin <alex.king.60137@gmai.com>
2021-01-28 17:44:13 -0500

    Add project files.

---
+++ Initialize.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FASBot
+{
+
+    public class Initialize
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _client;
+
+		// Ask if there are existing CommandService and DiscordSocketClient
+		// instance. If there are, we retrieve them and add them to the
+		// DI container; if not, we create our own.
+		public Initialize(CommandService commands = null, DiscordSocketClient client = null)
+		{
+			_commands = commands ?? new CommandService();
+			_client = client ?? new DiscordSocketClient();
+		}
+
+		public IServiceProvider BuildServiceProvider() => new ServiceCollection()
+			.AddSingleton(_client)
+			.AddSingleton(_commands)
+			// You can pass in an instance of the desired type
+			//.AddSingleton(new NotificationService())
+
+			// ...or by using the generic method.
+			//
+			// The benefit of using the generic method is that 
+			// ASP.NET DI will attempt to inject the required
+			// dependencies that are specified under the constructor 
+			// for us.
+			//.AddSingleton<DatabaseService>()
+			.AddSingleton<CommandHandler>()
+			.BuildServiceProvider();
+	}
+}
---
+++ IntroModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FASBot
+{
+    public class IntroModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo) => ReplyAsync(echo);
+    }
+}
