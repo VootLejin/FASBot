143d34b0e5735e50ea859ed16f40e6e946fb8249
Alexander King <AleKing@geico.com>
2021-02-01 12:14:40 -0500

    Create FasBotFramework ckass

---
+++ src/FasBotFramework.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Net.Rest;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FASBot
+{
+    public class FasBotFramework
+    {
+        private DiscordSocketClient _client;
+        private CommandHandler _commandHandler;
+        private CommandService _commands;
+
+        private readonly IServiceProvider _services;
+
+        public FasBotFramework()
+        {
+            var socketConfig = new DiscordSocketConfig
+            {
+                RestClientProvider = DefaultRestClientProvider.Create(useProxy: true)
+            };
+            _client = new DiscordSocketClient(socketConfig);
+            _commands = new CommandService();
+
+            _commandHandler = new CommandHandler(_client, _commands);
+
+
+            _client.Log += Log;
+            _commands.Log += Log;
+            _services = ConfigureServices();
+        }
+
+        public async Task MainAsync()
+        {
+
+            // Centralize the logic for commands into a separate method.
+            await InitCommands();
+
+            var token = File.ReadAllText("token.txt");
+            // Login and connect.
+            await _client.LoginAsync(TokenType.Bot,
+                // < DO NOT HARDCODE YOUR TOKEN >
+                token);
+            await _client.StartAsync();
+
+            // Wait infinitely so your bot actually stays connected.
+            await Task.Delay(Timeout.Infinite);
+
+
+
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            
+
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task InitCommands()
+        {
+            // Either search the program and add all Module classes that can be found.
+            // Module classes MUST be marked 'public' or they will be ignored.
+            // You also need to pass your 'IServiceProvider' instance now,
+            // so make sure that's done before you get here.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            // Or add Modules manually if you prefer to be a little more explicit:
+            //await _commands.AddModuleAsync<SomeModule>(_services);
+            // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        // If any services require the client, or the CommandService, or something else you keep on hand,
+        // pass them as parameters into this method as needed.
+        // If this method is getting pretty long, you can seperate it out into another file using partials.
+        private static IServiceProvider ConfigureServices()
+        {
+            var map = new ServiceCollection()
+                // Repeat this for all the service classes
+                // and other dependencies that your commands might need.
+                //.AddSingleton(new SomeServiceClass());
+                ;
+
+            // When all your required services are in the collection, build the container.
+            // Tip: There's an overload taking in a 'validateScopes' bool to make sure
+            // you haven't made any mistakes in your dependency graph.
+            return map.BuildServiceProvider();
+        }
+    }
+}
